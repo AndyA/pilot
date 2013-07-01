a5c1fcc05c2737a07e343b0c0f5bdd4734d60f97
Andy Armstrong <andy@hexten.net>
2013-07-01 22:51:50 +0100

    Initial checkin.

---
+++ pilot.h
@@ -0,0 +1,14 @@
+/* hlsm3u8.h */
+
+#ifndef HLSM3U8_H_
+#define HLSM3U8_H_
+
+#include <jsondata.h>
+#include <stdio.h>
+
+jd_var *hlsm3u8_parse(jd_var *out, FILE *in);
+
+#endif
+
+/* vim:ts=2:sw=2:sts=2:et:ft=c 
+ */
