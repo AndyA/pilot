9fa5043794ede3797fbcdfb7055a3d2b4b45d5c5
Andy Armstrong <andy@hexten.net>
2013-07-03 21:27:48 +0100

    Move refactoring to flyer2.c

--- flyer2.c
+++ flyer2.c
@@ -46,6 +46,27 @@ static void cwiid_callback(cwiid_wiimote_t *wiimote, int mesg_count,
   }
 }
 
+static jd_var *load_string(jd_var *out, FILE *f) {
+  char buf[512];
+  size_t got;
+
+  jd_set_empty_string(out, 10000);
+  while (got = fread(buf, 1, sizeof(buf), f), got)
+    jd_append_bytes(out, buf, got);
+  return out;
+}
+
+static jd_var *load_json(jd_var *out, FILE *f) {
+  jd_var json = JD_INIT;
+  jd_from_json(out, load_string(&json, f));
+  jd_release(&json);
+  return out;
+}
+
+static void open_worker(void *ctx) {
+  (void) ctx;
+}
+
 int main(int argc, char *argv[]) {
   bdaddr_t bdaddr;
   cwiid_wiimote_t *wiimote;
